d8963a2baaeaa5cc7ec090ad1a5fbfd74840f399
Erik Hedberg <erik@hedberg.at>
2022-09-22 20:08:42 +0200

    test retrieving client id in main

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"os"
+	"wr-latest-daily-redirect-serverless/pkg/daily"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	clientId := os.Getenv("REDDIT_CLIENT_ID")
+	if clientId == "" {
+		panic("REDDIT_CLIENT_ID not set")
+	}
+
+	clientSecret := os.Getenv("REDDIT_CLIENT_SECRET")
+	if clientSecret == "" {
+		panic("REDDIT_CLIENT_SECRET not set")
+	}
+	d := daily.NewDaily(clientId, clientSecret, "golang:wr-latest-daily-redirect-serverless:1.0.0 (by /u/murrtu)")
+	d.Posts()
 }
